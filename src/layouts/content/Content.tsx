ae9f599e7ba1b646b63cb78f8e575f732fbed57e
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2024-03-14 00:06:13 +0330

    add Content component

---
+++ src/layouts/content/Content.tsx
@@ -0,0 +1,29 @@
+import { Outlet } from 'react-router-dom';
+
+import { Flex, Stack, Container } from '@uiKits';
+
+export const Content = () => {
+    return (
+        <Flex
+            h={'100%'}
+            mt={'-60px'}
+        >
+            <Stack
+                flex={4}
+                pt={90}
+            >
+                <Container
+                    w={'100%'}
+                >
+                    <Outlet />
+                </Container>
+            </Stack>
+            <Stack
+                flex={2}
+                h={'100%'}
+                bg={'#fefbeb'}
+            />
+        </Flex>
+
+    );
+};
--- src/layouts/mainLayout/MainLayout.tsx
+++ src/layouts/mainLayout/MainLayout.tsx
@@ -1,7 +1,6 @@
-import { Outlet } from 'react-router-dom';
-
-import { Flex, Stack, Image, Container } from '@uiKits';
+import { Flex, Image } from '@uiKits';
 import { Header } from '../header';
+import { Content } from '../content';
 
 export const MainLayout = () => {
     return (
@@ -11,27 +10,7 @@ export const MainLayout = () => {
             h={'100vh'}
         >
             <Header />
-            <Flex
-                h={'100%'}
-                mt={'-60px'}
-            >
-                <Stack
-                    flex={4}
-                    pt={90}
-                >
-                    <Container
-                        w={'100%'}
-                    >
-                        <Outlet />
-                    </Container>
-                </Stack>
-                <Stack
-                    flex={2}
-                    h={'100%'}
-                    bg={'#fefbeb'}
-                />
-            </Flex>
-
+            <Content />
             <Image
                 src='/car-green.svg'
                 alt='car'
